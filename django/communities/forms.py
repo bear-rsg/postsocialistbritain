ad95eccc56f08338932960e115444a2dd87e42fa
Mike Allaway <36171746+mike-allaway@users.noreply.github.com>
2022-11-21 12:47:38 +0000

    community page enhanced with multiple image upload and displaying images in list view

--- django/communities/forms.py
+++ django/communities/forms.py
@@ -41,8 +41,8 @@ class StoryCreateForm(forms.ModelForm):
     story_text = forms.CharField(max_length=1000,
                                  widget=forms.Textarea(),
                                  label='Story text')
-    story_image = forms.ImageField(label='Upload image',
-                                   help_text='Optional',
+    story_image = forms.ImageField(label='Upload main image (select one)',
+                                   help_text='Optional. This main image will appear in stories list and detail page of each story.',
                                    required=False)
     author_name = forms.CharField(label='Name',
                                   widget=forms.TextInput(),
