b1482f3534642bbbc4ebae78e686fa61edda4613
Mike Allaway <mikeallaway@gmail.com>
2025-10-22 09:19:43 +0100

    fixed captcha

--- django/communities/forms.py
+++ django/communities/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from . import models
-from captcha.fields import ReCaptchaField
-from captcha.widgets import ReCaptchaV3
+from django_recaptcha.fields import ReCaptchaField
+from django_recaptcha.widgets import ReCaptchaV3
 
 
 class ResponseCreateForm(forms.ModelForm):
